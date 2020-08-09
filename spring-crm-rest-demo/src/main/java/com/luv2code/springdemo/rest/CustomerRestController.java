04c007b76843c716196a367db1f3dc9cb1dbec44
Denis Kisina <kisinad@gmail.com>
2020-08-08 19:55:48 -0500

    added getting single customer

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +20,18 @@ public class CustomerRestController {
 	private CustomerService customerService;
 
 	// add mapping for get customer
-	@GetMapping("customers")
+	@GetMapping("/customers")
 	public List<Customer> getCustomer() {
 
 		return customerService.getCustomers();
 
+	}
+
+	// add mapping for get customers/customerId
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		Customer theCustomer = customerService.getCustomer(customerId);
+
+		return theCustomer;
 	}
 }
